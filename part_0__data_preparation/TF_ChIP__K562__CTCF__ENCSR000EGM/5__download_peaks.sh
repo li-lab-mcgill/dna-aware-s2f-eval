31ea143d151f7bf0d82f7ecd372e3fa748006a41
Doruk Cakmakci <doruk.cakmakci@outlook.com>
2026-03-29 01:51:39 -0400

    Remove prior material entirely

--- part_0__data_preparation/TF_ChIP__K562__CTCF__ENCSR000EGM/5__download_peaks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-### NOTE: peaks are manually downloaded from ENCODE
-### https://www.encodeproject.org/files/ENCFF400DFR/
--- part_1__sequence2function/ATAC__GM12878__ENCSR637XSC/bpnet/1__test_gpu_access.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
-
-python -c "import tensorflow as tf; print(f\"TF version: {tf.__version__}\"); print(f\"GPU available: {len(tf.config.list_physical_devices('GPU')) > 0}\"); print(f\"CUDA built: {tf.test.is_built_with_cuda()}\")"
-
-
